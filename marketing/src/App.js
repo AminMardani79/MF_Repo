0a6689c99e3004905aa92cd9015b7f6a664c26aa
Amin Mardani <aminmardani1819@gmail.com>
2023-03-22 23:47:50 +0430

    feat: config container project with webpack

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import Landing from './components/Landing'
-import Pricing from './components/Pricing'
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
-    <Routes>
-            <Route exact path='/' element={<Landing/>}/>
-            <Route exact path='/pricing' element={<Pricing/>}/>
-        </Routes>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
