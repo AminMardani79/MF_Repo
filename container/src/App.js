f2e596110dd3bb2fdc8a4155e7dcb876be80470e
Amin Mardani <aminmardani1819@gmail.com>
2023-03-23 21:15:21 +0430

    feat: use mount function and shared dependencies

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
-
-console.log(mount);
+import { Link } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
 
 function App() {
-  return <div>Hello This is container</div>;
+  return (
+    <>
+      <div>
+        Hello This is container <Link to="/pricing">Pricing</Link>
+      </div>
+      <MarketingApp />
+    </>
+  );
 }
 
 export default App;
